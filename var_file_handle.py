2dee63450267d238125106aef4664d28425a6165
wsxtag <102475284+wsxtag@users.noreply.github.com>
2022-05-06 02:32:47 +0800

    增加正则表达式。

--- var_file_handle.py
+++ var_file_handle.py
@@ -12,6 +12,25 @@ def var_test():
 
             # print(type(f.infolist()))
             # print(f.infolist())
+
+            # print(type(f.namelist()))
+            # print(f.namelist())
+
+    # "scenes""saves/scene/.*?\x2e(?:json)"
+    # "looks" "saves/person/appearance/.*?\x2e(?:json|vac)",
+    # "looks""custom/atom/person/(?:general|appearance)/.*?\x2e(?:json|vap)",
+    # "clothing""custom/clothing/.*?\x2e(?:vam|vap)",
+    # "clothing""custom/atom/person/clothing/.*?\x2e(?:vam|vap)",
+    # "hairstyle""custom/hair/.*?\x2e(?:vam|vap)",
+    # "hairstyle""custom/atom/person/hair/.*?\x2e(?:vam|vap)",
+    # "plugin""custom/scripts/.*?\x2e(?:cs)",
+    # "plugin""custom/atom/person/scripts/.*?\x2e(?:cs)",
+    # "plugin""custom/scripts/.*?\x2e(?:cslist)",
+    # "plugin""custom/atom/person/scripts/.*?\x2e(?:cslist)",
+    # "assets""custom/assets/.*?\x2e(?:assetbundle)",
+
+
+
     except Exception as e:
         print("异常对象的类型是:%s" % type(e))
         print("异常对象的内容是:%s" % e)
