4a4db43ea1f0985833979778a7d2463c84fff9cb
wsxtag <102475284+wsxtag@users.noreply.github.com>
2022-05-13 06:13:41 +0800

    获取zip信息，读取出来做分类。

---
+++ var_directory_handle.py
@@ -0,0 +1,12 @@
+import os
+
+
+def get_var_list(vd):
+    return vd
+
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    # var_file_dir = "J:\\vam_vars\\___VarTidied___\\50shades\\50shades.Cum_pack_of_9.1.var"
+    var_dir = "J:\\vam_vars\\___VarTidied___\\AcidBubbles\\AcidBubbles.DraeneiFutaFemalePov.4.var"
+    print(get_var_list(var_dir))
--- var_file_handle.py
+++ var_file_handle.py
@@ -1,35 +1,72 @@
+import re
+import os
 import zipfile
+from typing import Any
 
-var_file_dir = "J:\\vam_vars\\___VarTidied___\\50shades\\50shades.Cum_pack_of_9.1.var"
+__REG_RULE__ = (
+    ("(custom/clothing/.*)(?<=/)([^/]+)\x2e(?:vam|vap)", "clothing"),
+    ("(custom/atom/person/clothing/.*)(?<=/)([^/]+)\x2e(?:vam|vap)", "clothing"),
+    ("(saves/scene/.*)(?<=/)([^/]+)\x2e(?:json)", "scene"),
+    ("(saves/person/appearance/.*)(?<=/)([^/]+)\x2e(?:json|vac)", "looks"),
+    ("(custom/atom/person/(?:general|appearance)/.*)(?<=/)([^/]+)\x2e(?:json|vap)", "looks"),
+    ("(custom/hair/.*)(?<=/)([^/]+)\x2e(?:vam|vap)", "hairstyle"),
+    ("(custom/atom/person/hair/.*)(?<=/)([^/]+)\x2e(?:vam|vap)", "hairstyle"),
+    ("(custom/scripts/.*)(?<=/)([^/]+)\x2e(?:cs)", "plugin"),
+    ("(custom/atom/person/scripts/.*)(?<=/)([^/]+)\x2e(?:cs)", "plugin"),
+    ("(custom/scripts/.*)(?<=/)([^/]+)\x2e(?:cslist)", "plugin"),
+    ("(custom/atom/person/scripts/.*)(?<=/)([^/]+)\x2e(?:cslist)", "plugin"),
+    ("(custom/assets/.*)(?<=/)([^/]+)\x2e(?:assetbundle)", "assets")
+)
 
 
-def var_test():
+def reg_test():
+    _txt = "J:/vam_vars/___VarTidied___/AcidBubbles/AcidBubbles.DraeneiFutaFemalePov.4.var"
+    _reg = ".*(?<=/)([^/]+)\x2e(?:var)"
+
+    _result = re.match(_reg, _txt, re.I)
+    if _result:
+        print(_result.group())
+        print(_result.group(1))
+        # print(_result.group(2))
+    else:
+        print("not macth")
+
+
+def get_var_mod(var):
+    var_data: dict[str, list[Any]] = {"clothing": [], "scene": [], "looks": [], "hairstyle": [], "plugin": [],
+                                      "assets": []}
+    package_name = "temp_var"
+
     try:
-        with zipfile.ZipFile(var_file_dir) as f:
-            f.printdir()
-            # print(type(f.getinfo('meta.json')))
-            # print(f.getinfo('meta.json'))
+        with zipfile.ZipFile(var) as f:
+            #验证包体合法性
 
-            # print(type(f.infolist()))
-            # print(f.infolist())
+            #读取模组
+            for i in f.namelist():
+                for j in __REG_RULE__:
+                    _result = re.match(j[0], i, re.I)
+                    if _result:
+                        var_data[j[1]].append(
+                            {"type": j[1], "name": _result.group(2), "path": _result.group(1), "preview": 0})
 
-            # print(type(f.namelist()))
-            # print(f.namelist())
+            #读取模组的预览图
+            for l in var_data:
+                if var_data[l]:
+                    for m in var_data[l]:
+                        _pic_path = m["path"] + m["name"] + ".jpg"
+                        try:
+                            fdata = f.read(_pic_path)
+                            # 如果存在预览图，则写回标志位
+                            m['preview'] = 1
+                            _target_site = os.getcwd() + "\\temp\\" + m["name"] + ".jpg"
+                            # _target_site = os.getcwd() + "\\___PreviewPics__\\" + m["type"] + "\\" + package_name + "\\" + m["name"] + ".jpg"
+                            try:
+                                (lambda ff, d: (ff.write(d), ff.close()))(open(_target_site, 'wb'), fdata)
+                            except Exception as e:
+                                print(e)
 
-    # "scenes""saves/scene/.*?\x2e(?:json)"
-    # "looks" "saves/person/appearance/.*?\x2e(?:json|vac)",
-    # "looks""custom/atom/person/(?:general|appearance)/.*?\x2e(?:json|vap)",
-    # "clothing""custom/clothing/.*?\x2e(?:vam|vap)",
-    # "clothing""custom/atom/person/clothing/.*?\x2e(?:vam|vap)",
-    # "hairstyle""custom/hair/.*?\x2e(?:vam|vap)",
-    # "hairstyle""custom/atom/person/hair/.*?\x2e(?:vam|vap)",
-    # "plugin""custom/scripts/.*?\x2e(?:cs)",
-    # "plugin""custom/atom/person/scripts/.*?\x2e(?:cs)",
-    # "plugin""custom/scripts/.*?\x2e(?:cslist)",
-    # "plugin""custom/atom/person/scripts/.*?\x2e(?:cslist)",
-    # "assets""custom/assets/.*?\x2e(?:assetbundle)",
-
-
+                        except Exception as e:
+                            print(e)
 
     except Exception as e:
         print("异常对象的类型是:%s" % type(e))
@@ -38,7 +75,12 @@ def var_test():
     finally:
         f.close()
 
+    return var_data
+
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    var_test()
+    # var_file_dir = "J:\\vam_vars\\___VarTidied___\\50shades\\50shades.Cum_pack_of_9.1.var"
+    var_file_path = "J:\\vam_vars\\___VarTidied___\\AcidBubbles\\AcidBubbles.DraeneiFutaFemalePov.4.var"
+    print(get_var_mod(var_file_path))
+
