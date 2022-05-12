1e53c41e6f679d6e57f2be8ae0b4f88f92375e8e
wsxtag <102475284+wsxtag@users.noreply.github.com>
2022-05-13 06:36:29 +0800

    定义获取var文件函数

--- var_directory_handle.py
+++ var_directory_handle.py
@@ -1,21 +1,21 @@
 import os
+from typing import Dict, Any
 
 
 def get_var_list(vd):
-    def find_all_file(vd):
-        for root, ds, fs in os.walk(base):
-            for f in fs:
-                if f.endswith('.var'):
-                    fullname = os.path.join(root, f)
-                    var_dictionary = {}
-                    var_dictionary[f] = fullname
-                    yield var_dictionary
+    for root, ds, fs in os.walk(vd):
+        for f in fs:
+            if f.endswith('.var'):
+                fullname = os.path.join(root, f)
+                var_dictionary: dict[Any, bytes | str] = {}
+                var_dictionary[f] = fullname
+                # yield var_dictionary
 
-    return vd
+    return var_dictionary
 
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     # var_file_dir = "J:\\vam_vars\\___VarTidied___\\50shades\\50shades.Cum_pack_of_9.1.var"
-    var_dir = "J:\\vam_vars\\___VarTidied___\\AcidBubbles\\AcidBubbles.DraeneiFutaFemalePov.4.var"
+    var_dir = "J:\\vam_vars\\___VarTidied___\\"
     print(get_var_list(var_dir))
